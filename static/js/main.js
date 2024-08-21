a5b3dcebcfdabdd48768915e84e1528c5267b292
mamouddiallo <diallomamoud298@gmail.com>
2024-08-21 19:59:56 +0000

    construction des differentes sections

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+document.addEventListener("DOMContentLoaded", function() {
+	// Sélectionner toutes les cartes avec la classe "card"
+	var cards = document.querySelectorAll(".card");
+  
+	// Ajouter un écouteur d'événement pour l'entrée de la souris (mouseenter) sur chaque carte
+	cards.forEach(function(card) {
+	  card.addEventListener("mouseenter", function() {
+		// Ajouter la classe "selected" à la carte courante
+		this.classList.add("selected");
+  
+		// Retirer la classe "selected" des cartes voisines (siblings)
+		cards.forEach(function(sibling) {
+		  if (sibling !== card) {
+			sibling.classList.remove("selected");
+		  }
+		});
+	  });
+	});
+  });
+  
+  
+  
