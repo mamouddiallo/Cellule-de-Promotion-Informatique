090ac9904ee0f2cdadcd423e9ba24350bf574428
mamouddiallo <diallomamoud298@gmail.com>
2024-10-29 22:16:07 +0000

    modif

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,7 @@ app_name = "webapp"
 urlpatterns = [
     path('', views.home, name="shop"),
     path('formations/', views.formations, name="formations"),
-    path('apropos/', views.apropos, name="apropos"),
+    path('apropos/', views.apropos, name='apropos'),  
     path('contact/', views.contact, name="contact"),
 ]
 
